0f05605b0c0ea88ecba8767c0b36b855a4b55d65
farrah-riaz <friaz4@my.centennialcollege.ca>
2025-04-04 03:21:39 -0400

    Add project files.

---
+++ StringExtensionsLibrary/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensionsLibrary
+{
+    // Class must be static for extension methods
+    public static class StringExtensions
+    {
+        // Extension method must also be static and use 'this' keyword before type
+        public static bool StartsWithUpper(this string str)
+        {
+            // Checks if the input is null or empty
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            // Check if the first character is uppercase
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
---
+++ TestStringExtensions/Program.cs
@@ -0,0 +1,7 @@
+﻿using StringExtensionsLibrary;
+
+string testString = "Hello World";
+Console.WriteLine($"Does '{testString}' start with an uppercase letter? {testString.StartsWithUpper()}");
+
+testString = "hello World";
+Console.WriteLine($"Does '{testString}' start with an uppercase letter? {testString.StartsWithUpper()}");
